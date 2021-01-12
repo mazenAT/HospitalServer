952b8ff43d686955382e65589e04934ac6d5442a
EzzatElshazly <ezzat169011@bue.edu.eg>
2021-01-12 04:17:09 +0200

    Adding classes Accountant,Doctor,Admin and receptionist

---
+++ src/hospital/Person/Admin.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital.Person;
+import hospital.Tranactions.Operation;
+/**
+ *
+ * @author Ezzat
+ */
+public class Admin extends person {
+    private int adminLicence;
+    private String Credintials;
+    private person[] person;
+    private Admin AdminInsurance;
+    private Operation operation;
+
+    public Admin(int adminLicence, String Credintials, person[] person, Admin AdminInsurance, Operation operation, String name, String email, String phone, int age, Genders gender, String address, String role, float salary) {
+        super(name, email, phone, age, gender, address, role, salary);
+        this.adminLicence = adminLicence;
+        this.Credintials = Credintials;
+        this.person = person;
+        this.AdminInsurance = AdminInsurance;
+        this.operation = operation;
+    }
+
+    public int getAdminLicence() {
+        return adminLicence;
+    }
+
+    public void setAdminLicence(int adminLicence) {
+        this.adminLicence = adminLicence;
+    }
+
+    public String getCredintials() {
+        return Credintials;
+    }
+
+    public void setCredintials(String Credintials) {
+        this.Credintials = Credintials;
+    }
+
+    public person[] getPerson() {
+        return person;
+    }
+
+    public void setPerson(person[] person) {
+        this.person = person;
+    }
+
+    public Admin getAdminInsurance() {
+        return AdminInsurance;
+    }
+
+    public void setAdminInsurance(Admin AdminInsurance) {
+        this.AdminInsurance = AdminInsurance;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" + "adminLicence=" + adminLicence + ", Credintials=" + Credintials + ", person=" + person + ", AdminInsurance=" + AdminInsurance + ", operation=" + operation + '}';
+    }
+
+   public Admin GetInstance(){
+       return AdminInsurance;
+   }
+    
+   public void Update(){
+       
+   }
+    
+}
