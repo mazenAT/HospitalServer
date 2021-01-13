0d2a74647270c0331f73ce24985a0723e8cc3125
mazen <mazenatef508@gmail.com>
2021-01-14 00:14:34 +0200

    correct version

--- src/hospital/HospitalServer.java
+++ src/hospital/HospitalServer.java
@@ -26,8 +26,8 @@ public class HospitalServer {
     public static void main(String[] args) {
         // TODO code application logic here
         DBConnection db = new DBConnection();
-        //Person N = new Nurse("Bachelors","Nightshift","xyz",false,"7osnya","7osnya.bue@7osnya.com","0121616532", 26,"Female","obour city","nurse",2000);
-        //db.insertPerson(N);
+        Person N = new Nurse("Bachelors","Nightshift","xyz",false,"7osnya","7osnya.bue@7osnya.com","0121616532", 26,"Female","obour city","nurse",2000);
+        db.insertPerson(N);
         
         try{
              ReceptionistManager R = new Reception();
