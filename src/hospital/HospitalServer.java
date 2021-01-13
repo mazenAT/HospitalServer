a362e5a8c75964e63f270bc2e5e549ee29fc4b22
mazen <mazenatef508@gmail.com>
2021-01-13 13:45:57 +0200

    some required changes

--- src/hospital/HospitalServer.java
+++ src/hospital/HospitalServer.java
@@ -25,7 +25,7 @@ public class HospitalServer {
         // TODO code application logic here
         DBConnection db = new DBConnection();
         Person N = new Nurse("Bachelors","Nightshift","xyz",false,"7osnya","7osnya.bue@7osnya.com","0121616532", 26,"Female","obour city","nurse",2000);
-        db.deletePerson("7osnya.bue@7osnya.com");
+        db.insertPerson(N);
     }
     
 }
